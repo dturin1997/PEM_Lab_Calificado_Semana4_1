0abcd1909228388eeead912ff0fd0ff2f0cd73cb
Dario Turin <dturin1997@gmail.com>
2022-04-23 14:26:13 -0500

    "Commit Laboratorio de la semana 05"

--- app/src/main/java/com/turin/pem_lab_calificado_semana4_1/DetailActivity.kt
+++ app/src/main/java/com/turin/pem_lab_calificado_semana4_1/DetailActivity.kt
@@ -3,7 +3,7 @@ package com.turin.pem_lab_calificado_semana4_1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_detail.*
+import com.turin.pem_lab_calificado_semana4_1.databinding.ActivityDetailBinding
 
 const val PARAMETER_EXTRA_NOMBRE = "nombre"
 const val PARAMETER_EXTRA_CORREO = "correo"
@@ -11,40 +11,48 @@ const val PARAMETER_EXTRA_OFICINA = "oficina"
 const val PARAMETER_EXTRA_NUMERO = "numero"
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+
+        val view = binding.root
+
+        setContentView(view)
 
         val extras = this.intent.extras
 
         if (extras != null) {
             if (extras.get(PARAMETER_EXTRA_NOMBRE) != null) {
-                edtNombre.setText(extras.getString(PARAMETER_EXTRA_NOMBRE))
-
+                binding.edtNombre.setText(extras.getString(PARAMETER_EXTRA_NOMBRE))
             }
 
             if (extras.get(PARAMETER_EXTRA_CORREO) != null) {
-                edtCorreo.setText(extras.getString(PARAMETER_EXTRA_CORREO))
+                binding.edtCorreo.setText(extras.getString(PARAMETER_EXTRA_CORREO))
             }
 
             if (extras.get(PARAMETER_EXTRA_OFICINA) != null) {
-                edtOficina.setText(extras.getString(PARAMETER_EXTRA_OFICINA))
+                binding.edtOficina.setText(extras.getString(PARAMETER_EXTRA_OFICINA))
             }
             if (extras.get(PARAMETER_EXTRA_NUMERO) != null) {
-                edtNumero.setText(extras.getString(PARAMETER_EXTRA_NUMERO))
+                binding.edtNumero.setText(extras.getString(PARAMETER_EXTRA_NUMERO))
             }
         }
 
+        listenerCallButtons()
     }
 
-    fun closeActivity(view: android.view.View) {
-        val intent = Intent()
-            intent.putExtra("nombre", edtNombre.getText().toString())
-            intent.putExtra("correo", edtCorreo.getText().toString())
-            intent.putExtra("oficina", edtOficina.getText().toString())
-            intent.putExtra("numero", edtNumero.getText().toString())
-        setResult(RESULT_OK, intent) // Set ResultCode and DataIntent
-        finish()
-
+    fun listenerCallButtons() {
+        binding.btnGuardar.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("nombre", binding.edtNombre.getText().toString())
+            intent.putExtra("correo", binding.edtCorreo.getText().toString())
+            intent.putExtra("oficina", binding.edtOficina.getText().toString())
+            intent.putExtra("numero", binding.edtNumero.getText().toString())
+            setResult(RESULT_OK, intent) // Set ResultCode and DataIntent
+            finish()
+        }
     }
 }
